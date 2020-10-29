c04195347cec6f665fa13f157c5e95d64509799e
R <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-10-29 17:53:35 +0100

    asasdd

--- tools/validate-locale.js
+++ tools/validate-locale.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 
-const contents = fs.readdirSync('../');
+const contents = fs.readdirSync("./");
 
 console.dir(contents);
 
-/*const filechanges = JSON.parse(fs.readFileSync("../files.json"));
+/*
+
+const filechanges = JSON.parse(fs.readFileSync("../files.json"));
 
 filechanges.forEach((name) => {
   if (name.indexOf("nl.json") < -1 && name.indexOf("en.json") < -1) {
@@ -12,6 +14,6 @@ filechanges.forEach((name) => {
       "Only the files nl.json and/or en.json can be changed by a Lokalize PR."
     );
   }
-});/*
+});*/
 
 throw new Error("I really want to fail!");
