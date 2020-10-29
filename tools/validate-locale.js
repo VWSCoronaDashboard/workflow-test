0c8b239763f30b048b4d493c42a11abc6ecc60fd
R <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-10-29 17:45:18 +0100

    add locale checker

---
+++ tools/validate-locale.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const filechanges = JSON.parse(fs.readFileSync("../files.json"));
+
+filechanges.forEach((name) => {
+  if (name.indexOf("nl.json") < -1 && name.indexOf("en.json") < -1) {
+    throw new Error(
+      "Only the files nl.json and/or en.json can be changed by a Lokalize PR."
+    );
+  }
+});
+
+throw new Error("I really want to fail!");
