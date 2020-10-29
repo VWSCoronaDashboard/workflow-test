60b89e1cb97f661977c122716fe78c1fa5d8c900
R <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-10-29 18:26:08 +0100

    sfdsffd

--- tools/validate-locale.js
+++ tools/validate-locale.js
@@ -1,18 +1,5 @@
 const fs = require("fs");
 const path = require("path");
 
-const contents = fs.readdirSync(path.join("home", "runner", "files.json"));
-
-console.dir(contents);
-
-/*const filechanges = JSON.parse(fs.readFileSync("../files.json"));
-
-filechanges.forEach((name) => {
-  if (name.indexOf("nl.json") < -1 && name.indexOf("en.json") < -1) {
-    throw new Error(
-      "Only the files nl.json and/or en.json can be changed by a Lokalize PR."
-    );
-  }
-});*/
-
-throw new Error("I really want to fail!");
+console.dir(fs);
+console.dir(path);
